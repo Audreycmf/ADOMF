f83e17c16311506b15d84e96be68c7283a23ad44
Audrey <audreymoret@hotmail.fr>
2018-05-05 18:47:36 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<div id="footer-wrapper">
+  <footer id="footer" class="container">
+    <div class="row">
+      <div class="8u 12u(mobile)">
+
+        <!-- Contact -->
+          <section>
+            <h2><a href="contact.php">Contact</a></h2>
+            <div>
+              <div class="row">
+                <div class="6u 12u(mobile)">
+                  <ul class="link-list last-child">
+                    <li>Avenue des Tamaris 13616 Aix-en-Provence</li>
+                  </ul>
+                </div>
+                <div class="6u 12u(mobile)">
+                  <ul class="link-list last-child">
+                    <li><a href="tel:+33442335097">04 42 33 50 97</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+
+      </div>
+      <div class="4u 12u(mobile)">
+
+        <!-- Mentions légales -->
+          <section>
+            <h2><a href="mentions_legales.php">Mentions légales</a></h2>
+          </section>
+
+      </div>
+    </div>
+  </footer>
+</div>
+
+<!-- Copyright -->
+<div id="copyright">
+  &copy; A.D.O.M.F. All rights reserved.
+</div>
---
+++ header.php
@@ -0,0 +1,43 @@
+<div id="header-wrapper">
+  <header id="header" class="container">
+    <div class="row">
+      <div class="12u">
+
+        <!-- Logo -->
+          <h1><a href="index.php" id="logo">A.D.O.M.F.</a></h1>
+
+        <!-- Nav -->
+          <nav id="nav">
+            <a href="index.php">Accueil</a>
+            <a href="equipe.php">L'équipe</a>
+            <a href="interventions.php">Les interventions</a>
+            <a href="actualites.php">Actualités</a>
+            <a href="contact.php">Contact</a>
+          </nav>
+
+      </div>
+    </div>
+  </header>
+  <div id="banner">
+    <div class="container">
+      <div class="row">
+        <div class="6u 12u(mobile)">
+
+          <!-- Banner Copy -->
+            <h1><span class="maj">A</span>ssociation des <span class="maj">D</span>isciplines <span class="maj">O</span>rthodontiques
+              et <span class="maj">M</span>axillo-<span class="maj">F</span>aciales</h1>
+            <p>Avenue des Tamaris <br />
+              13616 Aix-en-Provence</p>
+            <a href="tel:+33442335097" class="link-list">04 42 33 50 97</a>
+
+        </div>
+        <div class="6u 12u(mobile)">
+
+          <!-- Banner Image -->
+            <a href="index.html"><img src="images/logo_blanc_rouge.png" alt="" /></a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
