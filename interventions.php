43207e3d9a527575a05acea9b2919c2d51df28f4
Unknown <audreymoret@hotmail.fr>
2018-05-06 18:22:40 +0200

    création interventions

---
+++ interventions.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Les interventions</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+	      <?php include("header.php") ?>
+
+			<!-- Content -->
+				<div id="content-wrapper">
+					<div id="content">
+						<div class="container">
+							<div class="row">
+								<div class="12u">
+
+									<!-- Main Content -->
+										<section>
+											<header>
+												<h2>Les interventions</h2>
+											</header>
+											<p>
+												Les différents interventions pratiquées par un chirurgien maxillo-facial sont les suivantes :
+												<ul class="check-list">
+													<li><a href="orthognatique.php">La chirurgie orthognatique</a></li>
+													<li><a href="implantologie.php">L'implantologie</a></li>
+													<li><a href="cutanee.php">La chirurgie cutanée</a></li>
+													<li><a href="orbito_palpebral.php">La chirurgie orbito-palpébrale</a></li>
+													<li><a href="stomatologie.php">La stomatologie</a></li>
+													<li><a href="esthetique.php">La chirurgie esthétique</a></li>
+													<li><a href="preimplantaire.php">La chirurgie pré-implantaire</a></li>
+													<li><a href="glandes_salivaires.php">La chirurgie des glandes salivaires</a></li>
+													<li><a href="saos.php">La prise en charge du syndrome d'apnées du sommeil</a></li>
+													<li><a href="humanitaire.php">La chirurgie lors de missions humanitaires</a></li>
+												</ul>
+											</p>
+										</section>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<?php include("footer.php") ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
