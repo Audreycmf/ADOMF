ae4d207f87650eb430cae7b73cba1f3659bba715
Unknown <audreymoret@hotmail.fr>
2018-05-06 08:19:35 +0200

    Mentions légales

---
+++ mentions_legales.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Mentions légales</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="subpage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+        <?php include("header.php") ?>
+
+			<!-- Content -->
+				<div id="content-wrapper">
+					<div id="content">
+						<div class="container">
+							<div class="row">
+								<div class="12u">
+
+									<!-- Main Content -->
+										<section>
+											<header>
+												<h2>Mentions légales</h2>
+											</header>
+											<p>
+                        L'utilisateur en ayant accès à ce site s'engage à se conformer aux présentes conditions d'utilisation.
+                      </p>
+                      <p>
+                        Dernière mise à jour du site : 06/05/2018
+                      </p>
+                      <h3>Informations éditeur</h3>
+                      <p>Ce site est édité par :<br />
+                         Association Adomf<br />
+                         Centre Hospitalier du Pays d'Aix<br />
+                         Avenue des Tamaris<br />
+                         13616 Aix en Provence cedex 1<br />
+                      </p>
+                      <h3><a href="statuts.php">Lire les statuts</a></h3>
+                      <h3>Informations hébergeur</h3>
+                      <p>
+                        Ce site est hébergé par : phpnet.org
+                      </p>
+                      <h3>Informations webmaster</h3>
+                      <p>
+                        Le webmaster du site est le Dr Audrey Moret. Vous pouvez la contacter via le formulaire de contact dans l'oglet "Contact"
+                      </p>
+                      <h3>Informations médicales</h3>
+                      <p>
+                        Ce site s'adresse au grand public. Il apporte une information détaillée sur les principales interventions en chirurgie plastique et en
+                        chirurgie esthétique. Les informations médicales contenues sur le site ne peuvent se substituer à une consultation auprès d'un chirurgien
+                        qualifié en chirurgie plastique reconstructrice et esthétique. Ces informations ont été rédigées par les Dr Pierre Revol et Dr Audrey Moret,
+                        médecins spécialisés en chirurgie maxillo-faciale et stomatologie, ainsi que le Dr Julie Garçonnet, médecin qualifiée en chirurgie orale,
+                        inscrits au conseil de l'ordre des médecins des Bouches du Rhône. Les informations apportées par d'autres auteurs y sont mentionnées.
+                      </p>
+                      <h3>Droits d'auteur</h3>
+                      <p>
+                        Les informations présentes sur ce site peuvent être téléchargées, reproduites, imprimées sous réserve de :
+                        <ul class="basic-list">
+                         <li>n'utiliser de telles informations qu'à des fins personnelles et en aucune manière à des fins commerciales,</li>
+                         <li>ne pas modifier de telles informations,</li>
+                         <li>reproduire sur toutes copies la mention des droits d'auteur de l'éditeur.</li>
+                        </ul>
+                        Toute autre utilisation non expressément autorisée ou toute représentation totale ou partielle de ce site, par quelque procédé que ce soit,
+                        est strictement interdite sans autorisation préalable et écrite de l'éditeur et constituerait une contrefaçon sanctionnée par les articles
+                        L 355-2 et suivants du Code de la Propriété Intellectuelle.
+                      </p>
+                      <h3>Informatique et Libertés</h3>
+                      <p>
+                        Conformément à la loi n° 78-17 du 6 janvier 1978, relative à l'informatique, aux fichiers et aux libertés (dite loi "Informatique et Libertés"),
+                        ce site ainsi que le traitement automatisé des données nominatives réalisées à partir de ce site ont fait l'objet de déclarations à la CNIL.
+                        L'utilisateur de ce site est notamment informé que, conformément à la loi Informatique et Libertés, les informations qu'il communique sur les
+                        formulaires présents sur le site, sont nécessaires pour répondre à sa demande, et sont destinées à l'éditeur, responsable du traitement, à
+                        des fins de gestion administrative et médicale. En cas d'incident lié au dossier de l'utilisateur, un traitement spécifique pourra être
+                        réalisé.
+                      </p>
+                      <h3>Exercice du droit d'accès</h3>
+                      <p>
+                        Conformément à la Loi Informatique et Libertés, l'utilisateur de ce site dispose d'un droit d'accès et de rectification aux données qui le
+                        concernent. Pour toute demande, l'utilisateur doit écrire à : Association Adomf Centre Hospitalier du Pays d'Aix Avenue des Tamaris 13616
+                        Aix en Provence cedex 1.
+                      </p>
+                      <h3>Responsabilité</h3>
+                      <p>
+                        L'ensemble des informations accessibles via ce site est fourni en l'état. L'éditeur ne donne aucune garantie, explicite ou implicite, et
+                        n'assume aucune responsabilité relative à l'utilisation de ces informations. L'utilisateur est seul responsable de l'utilisation de telles
+                        informations. L'éditeur se réserve le droit de modifier à tout moment les présentes notamment en actualisant ce site. L'éditeur ne pourra
+                        être responsable pour quel que dommage que ce soit tant direct qu'indirect, résultant d'une information contenue sur ce site. L'éditeur met
+                        tout en œuvre pour offrir aux utilisateurs des informations et/ou des outils disponibles et vérifiés, mais ne saurait être tenue pour
+                        responsable des erreurs, d'une absence de disponibilité des informations et/ou de la présence de virus sur son site. Les sites extérieurs
+                        ayant un lien hypertexte avec le présent site ne sont pas sous contrôle de l'éditeur qui décline par conséquent toute responsabilité quant
+                        à leur contenu. L'utilisateur est seul responsable de leur utilisation.
+                      </p>
+                      <h3>Lien hypertexte</h3>
+                      <p>
+                        La création de liens hypertexte vers le site est soumise à l'accord préalable et écrit de l'éditeur.
+                      </p>
+                      <h3>Confidentialité</h3>
+                      <p>
+                        Les informations personnelles concernant les visiteurs de ce site, y compris leur identité, sont confidentielles. Le responsable du site
+                        s'engage sur l'honneur à respecter les conditions légales de confidentialité applicables en France et à ne pas divulguer ces informations à
+                        des tiers.
+                      </p>
+                      <h3>Financement du site/Publicité</h3>
+                      <p>
+                        Le financement du site est entièrement assurée par l'association ADOMF afin d'assurer son fonctionnement. La publicité n'est pas admise sur
+                        le site et ne constitue pas une source de financement. Les ressources de l'association sont définies dans l'article 15 de ses statuts
+                        (cf. supra).
+											</p>
+										</section>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<?php include("footer.php") ?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
